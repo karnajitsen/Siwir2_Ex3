3ff23c558d3ae3cffefa2a5444776dcfb6bc242d
karnajitsen <karnajitsen@gmail.com>
2015-06-27 15:05:05 +0200

    add

--- Grid.h
+++ Grid.h
@@ -8,6 +8,7 @@
 #include<malloc.h>
 #define LD 16
 #define ALLIGNMENT 32
+#define CELLS 14
 using namespace std;
 //#define M_PI 3.14
 class Grid
@@ -15,51 +16,50 @@ class Grid
 
     //__declspec(align(128))
     double * __restrict data = NULL;
-    size_t sizeX, sizeY, ld, totLength;
-    double hx, hy;
-
+    size_t sizeX, sizeY, ld, size;
+    
 public:
     explicit Grid()
     {
         data = (double*)memalign(ALLIGNMENT, 0);
         sizeX = 0;
         sizeY = 0;
-        hx = 0.0;
-        hy = 0.0;
+		size = 0.0;
     }
 
-    explicit Grid(const size_t x, const size_t y, const double& _hx, const double& _hy , bool bndrYN , bool isNeumann)
+	explicit Grid(const size_t x, const size_t y, const double* fi, const double ux, const double uy, const double rho)
     {
-        sizeX = x;
-        sizeY = y;
-        hx = _hx;
-        hy = _hy;
-        ld = x + LD;
-        totLength = (x - 2)*(y - 2);
-        data = (double*) memalign(ALLIGNMENT, ld*y*sizeof(double));
+        sizeX = x+2;
+        sizeY = y+2;
+		ld = sizeX + LD;
+		size = CELLS*ld*y*sizeof(double);
+        data = (double*) memalign(ALLIGNMENT, CELLS*ld*y*sizeof(double));
         //data = (double*) _aligned_malloc(ld*y*sizeof(double), ALLIGNMENT);
-        if (bndrYN && !isNeumann)
-        {
-            double l = (sizeX - 1.0)*hx;
-            for (int j = 0.0; (size_t)j < sizeX; j++)
-            {
-                double k = j*hx;
-                data[j] = gxy1(k, 0.0);
-                data[j*ld] = gxy1(0.0, k);
-                data[j + ld * (sizeX - 1)] = gxy1(k, l);
-                data[j * ld + (sizeX - 1)] = gxy1(l, k);
-            }
-        }
+		for (size_t i = 0; i < sizeY; i++)
+		{
+			for (size_t j = 0; j < sizeY; j++)
+			{
+				
+				data[i * ld + j * CELLS + 0] = fi[0];
+				data[i * ld + j * CELLS + 1] = fi[1];
+				data[i * ld + j * CELLS + 2] = fi[2];
+				data[i * ld + j * CELLS + 3] = fi[3];
+				data[i * ld + j * CELLS + 4] = fi[4];
+				data[i * ld + j * CELLS + 5] = fi[5];
+				data[i * ld + j * CELLS + 6] = fi[6];
+				data[i * ld + j * CELLS + 7] = fi[7];
+				data[i * ld + j * CELLS + 8] = fi[8];
+				data[i * ld + j * CELLS + 9] = ux;
+				data[i * ld + j * CELLS + 10] = uy;
+				data[i * ld + j * CELLS+12] = 0.0;
+				if (i == 0) data[i * ld + j * CELLS+12] = 1.0;
+				if (j == 0) data[i * ld + j * CELLS+12] = 4.0;
+				if (j == sizeX - 1)  data[i * ld + j * CELLS+12] = 2.0;
+				if (i == sizeY - 1) data[i * ld + j * CELLS+12] = 3.0;
+				data[i * ld + j * CELLS + 11] =  rho;
+			}
+		}
 
-        if (bndrYN && isNeumann)
-        {
-            for (int j = 1.0; (size_t)j < sizeX-1; j++)
-            {
-                double k = (j-1)*hx;
-                data[j] = gxy2(k);
-                data[j + ld * (sizeY - 1)] = gxy2(k);
-            }
-        }
         //data++;
     }
     ~Grid()
@@ -68,17 +68,12 @@ public:
         free(data);
     }
 
-    inline double gxy1(const double x, const double y)
-    {
-        return sin(M_PI * x) * sinh(M_PI * y);
-    }
+	inline void copy(Grid grd)
+	{
+		memcpy((void *)data, (void *)&grd(0, 0, 0), size);
+	}
 
-    inline double gxy2(const double x)
-    {
-        return (x * (1.0 - x));
-    }
-
-    inline void reset()
+    /*inline void reset()
     {
         for (size_t i = 0; i < sizeY; i++)
         {
@@ -87,20 +82,25 @@ public:
                 data[i*ld + j] = 0.0;
             }
         }
-    }
+    }*/
 
-    inline double& operator()(const size_t x, const size_t y)
+	inline void swap(const Grid tgtgrd)
+	{
+
+	}
+
+    inline double& operator()(const size_t x, const size_t y, const size_t f)
     {
         assert(x < sizeX);
         assert(y < sizeY);
-        return data[y*ld + x];
+        return data[y*ld + x * CELLS + f];
     }
 
-    inline double& operator()(const size_t x, const size_t y) const
+	inline double& operator()(const size_t x, const size_t y, const size_t f) const
     {
         assert(x < sizeX);
         assert(y < sizeY);
-        return data[y*ld + x];
+		return data[y*ld + x * CELLS + f];
     }
 
     inline Grid * operator+=(const Grid * rhs)
@@ -123,19 +123,5 @@ public:
         return sizeY;
     }
 
-    inline size_t getTotLength() const
-    {
-        return totLength;
-    }
-
-    inline double getHx() const
-    {
-        return hx;
-    }
-
-    inline double getHy() const
-    {
-        return hy;
-    }
-
+   
 };
--- lbm.cpp
+++ lbm.cpp
@@ -7,11 +7,100 @@
 #include "Grid.h"
 typedef double Real;
 
-
+#define D 2
+#define Q 9
 //Global Variables
 size_t sizex, sizey, timesteps;
 Real omega;
 
+Grid fluid, tmpfluid;
+double uw[2] = { (0.8, 0) };
+double disvel[Q][2] = { (0.0, 0.0), (1.0, 0.0), (0.0, 1.0), (-1.0, 0.0), (0.0, -1.0), (1.0, 1.0), (-1.0, 1.0), (-1.0, -1.0), (1.0, -1.0) };
+int neighbours[Q][2] = { (0, 0), (1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (-1, 1), (-1, -1), (1, -1) };
+double stencil[Q] = { 1.0 / 9.0, 4.0 / 9.0, 4.0 / 9.0, 4.0 / 9.0, 4.0 / 9.0, 1.0 / 36.0, 1.0 / 36.0, 1.0 / 36.0, 1.0 / 36.0 };
+
+inline void init()
+{
+	double ux = 0.0, uy = 0.0, initrho =1.0;
+	fluid = Grid(sizex, sizey, stencil, ux, uy, initrho);
+	tmpfluid = Grid(sizex, sizey, stencil, ux, uy, initrho);
+	/*for (int i = 0; i < Q; i++)
+	feq[i] = stencil[i];*/
+}
+
+inline double calLidVel(size_t k)
+{
+	return (6.0 * stencil[k] * (disvel[k][0] * uw[0] + disvel[k][1] * uw[1]));
+}
+
+inline void stream()
+{
+	for (size_t i = 1; i <= sizey; i++)
+	{
+		for (size_t j = 1; j <= sizex; j++)
+		{
+			for (int k = 1; k < Q; k++)
+			{
+				double pt = tmpfluid(i + neighbours[k][0], j + neighbours[k][1], 12);
+				if (pt > 0.0 || pt != 2.0 )
+					tmpfluid(i, j, ((k-1 + (Q - 1) / 2) % (Q - 1)) + 1) = fluid(i, j, k);       // Bounce back from vertical and bottom walls
+				if (pt == 2.0)
+					tmpfluid(i, j, ((k-1 + (Q - 1) / 2) % (Q - 1)) + 1) = fluid(i, j, k) - calLidVel(k); // Streaming effect due to lid velocity in upper wall
+				else
+				    tmpfluid(i + neighbours[k][0], j + neighbours[k][1], k+1) = fluid(i,j,k);  // Free Streaming
+				
+			}
+
+			tmpfluid(i, j, 0) = fluid(i, j, 0);
+		}
+	}
+}
+
+inline void calLatticeRhoVelocity()
+{
+	double rh=0.0, vx = 0.0, vy =0.0;
+	for (size_t i = 1; i <= sizey; i++)
+	{
+		for (size_t j = 1; j <= sizex; j++)
+		{
+			for (int k = 0; k < Q; k++)
+			{
+				rh += tmpfluid(i, j, k);
+				vx += tmpfluid(i, j, k) * disvel[k][0];
+				vy += tmpfluid(i, j, k) * disvel[k][1];
+			}
+
+			tmpfluid(i, j, 9) = vx / rh;
+			tmpfluid(i, j, 10) = vy / rh;
+			tmpfluid(i, j, 11) = rh;
+
+		}
+	}
+
+}
+
+inline double feq(size_t const k, double ux, double const uy, double const rho)
+{
+	double r = (ux * disvel[k][0] + uy * disvel[k][1]);
+	double u2 = ux * ux + uy * uy;
+	return stencil[k] * (rho + 3.0 * r + 4.5 * r*r - 1.5 * u2);
+}
+
+inline void collide()
+{
+	for (size_t i = 1; i <= sizey; i++)
+	{
+		for (size_t j = 1; j <= sizex; j++)
+		{
+			for (int k = 0; k < Q; k++)
+			{
+				tmpfluid(i, j, k) = (1.0 - omega) * tmpfluid(i, j, k) + feq(k, tmpfluid(i, j, 9), tmpfluid(i, j, 10), tmpfluid(i, j, 11));
+			}
+		}
+
+	}
+
+}
 
 int main(int argc, char** argv)
 {
@@ -38,6 +127,19 @@ int main(int argc, char** argv)
 
 	paramfile >> tmp;
 	paramfile >> omega;
+
+
+	init();
+
+	for (size_t i = 0; i < timesteps; i++)
+	{
+		stream();
+		calLatticeRhoVelocity();
+		collide();
+		fluid.copy(tmpfluid);
+	}
+
+
 
 
 
