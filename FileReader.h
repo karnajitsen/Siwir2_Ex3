0d8c7927e516f5793dfb97953dd7144d7011df2c
karnajitsen <karnajitsen@gmail.com>
2015-06-27 21:49:31 +0200

    add

--- FileReader.h
+++ FileReader.h
@@ -16,8 +16,29 @@ using namespace std;
 	class FileReader
 	{
 		public:
-			FileReader();
-			void readParameters(string filename);
+			FileReader::FileReader()
+			{
+
+			}
+			void FileReader::readParameters(string filename)
+			{
+				ifstream input;
+				string line;
+
+				input.open(filename, ios::in);
+				if (input.is_open())
+				{
+					while (getline(input, line))
+					{
+						size_t space = line.find_first_of(' ');
+						dat param;
+						param.key = line.substr(0, space);
+						param.value = line.substr(space + 1);
+						m_params.push_back(param);
+					}
+					input.close();
+				}
+			}
 			
 			template<typename Type>
 			Type getParameter(string valueName)
